0e8ed3b6efeab27ab1f9a3e89ca275649857bac0
NeilTheSeal <neil.hendren@gmail.com>
2024-05-31 14:50:59 -0600

    Add test for quiz result class method

--- spec/models/quiz_result_spec.rb
+++ spec/models/quiz_result_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe QuizResult, type: :model do
   it { should validate_presence_of :number_correct }
   it { should validate_numericality_of :number_correct }
   it { should validate_presence_of :user_id }
+
+  describe ".high_scores" do
+    it "returns the top 10 scores" do
+      user = create(:user)
+      11.times do |i|
+        create(:quiz_result, number_correct: i, user:)
+      end
+
+      high_scores = QuizResult.high_scores
+
+      expect(high_scores.length).to eq 10
+      expect(high_scores.first.score).to eq 10
+      expect(high_scores.last.score).to eq 1
+      expect(high_scores.first.username).to eq user.name
+    end
+  end
 end
