d11ec2ddfa5ae22422d06dccdb58ba450b7737c0
NeilTheSeal <neil.hendren@gmail.com>
2024-06-05 13:35:39 -0600

    Modify db schema

--- db/migrate/20240531154053_create_quiz_results.rb
+++ db/migrate/20240531154053_create_quiz_results.rb
@@ -2,7 +2,7 @@ class CreateQuizResults < ActiveRecord::Migration[7.1]
   def change
     create_table :quiz_results do |t|
       t.integer :number_correct
-      t.references :user, null: false, foreign_key: true
+      t.string :user_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,18 +16,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_31_154053) do
 
   create_table "quiz_results", force: :cascade do |t|
     t.integer "number_correct"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_quiz_results_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "user_id_token"
+    t.string "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "quiz_results", "users"
 end
