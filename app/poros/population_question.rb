a2ab18bdb774c8e257b78b0435967731a999cfe4
NeilTheSeal <neil.hendren@gmail.com>
2024-06-06 16:24:28 -0600

    Refactor population question to adhere to SRP

--- app/poros/population_question.rb
+++ app/poros/population_question.rb
@@ -9,15 +9,19 @@ class PopulationQuestion < QuizQuestion
   def generate_question
     countries = @geography_data.sample(4)
 
-    population = countries[0].population.to_s.reverse.gsub(
-      /(\d{3})(?=\d)/,
-      '\\1,'
-    ).reverse
+    population = population_with_commas(countries[0].population)
 
     @question = "What country has the population of #{population}?"
 
     @correct_answer = countries[0].name[:common]
     @options = countries.map { |country| country.name[:common] }
     @options.shuffle!
+  end
+
+  def population_with_commas(population)
+    population.to_s.reverse.gsub(
+      /(\d{3})(?=\d)/,
+      '\\1,'
+    ).reverse
   end
 end
